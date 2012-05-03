fd2805bfc96a08ab4329f75a910ee534f6253dde
Brooke Kuhlmann <brooke@redalchemist.com>
2012-05-02 21:30:24 -0600

    Added Homebrew formulas to README. Added missing installed to software.sh script.

--- scripts/software.sh
+++ scripts/software.sh
@@ -30,12 +30,20 @@ export CHROME_URL=https://dl.google.com/chrome/mac/stable/GGRO
 export CHROME_FILE=googlechrome.dmg
 export OPERA_URL=http://get3.opera.com/pub/opera/mac/1162
 export OPERA_FILE=Opera_11.62_Setup_Intel.dmg
+export OMNIFOCUS_URL=http://downloads.omnigroup.com/software/MacOSX/10.4
+export OMNIFOCUS_FILE=OmniFocus-1.10.1.dmg
+export OMNIOUTLINER_URL=http://downloads.omnigroup.com/software/MacOSX/10.4
+export OMNIOUTLINER_FILE=OmniOutliner-3.10.4.dmg
+export OMNIGRAFFLE_URL=http://downloads.omnigroup.com/software/MacOSX/10.5
+export OMNIGRAFFLE_FILE=OmniGrafflePro-5.3.6.dmg
 export OMNIDAZZLE_URL=http://downloads.omnigroup.com/software/MacOSX/10.4
 export OMNIDAZZLE_FILE=OmniDazzle-1.1.1.dmg
 export EXPANDRIVE_URL=http://downloads.expandrive.com/expandrive/v2-3-1_published_2012-03-07_at_15_18_52
 export EXPANDRIVE_FILE=ExpanDrive.zip
 export ISTATS_URL=http://s3.amazonaws.com/bjango/files/istatmenus3
 export ISTATS_FILE=istatmenus3.21.zip
+export AIR_DISPLAY_URL=http://avatron.com/downloads
+export AIR_DISPLAY_FILE=Air%20Display%20Installer.dmg
 export PASTEBOT_SYNC_URL=http://tapbots.net/pastebot
 export PASTEBOT_SYNC_FILE=PastebotSync.dmg
 export GITHUB_URL=https://github-central.s3.amazonaws.com
@@ -46,6 +54,8 @@ export ESPRESSO_URL=http://macrabbit.com/espresso/downloads
 export ESPRESSO_FILE=Espresso%202.0.1.dmg
 export CODEKIT_URL=http://incident57.com/codekit/files
 export CODEKIT_FILE=codekit-6166.zip
+export ACORN_URL=http://flyingmeat.com/download
+export ACORN_FILE=Acorn.zip
 export DOUBLETAKE_URL=http://echoone.com/doubletake
 export DOUBLETAKE_FILE=DoubleTake.dmg
 export IMAGE_OPTIM_URL=http://imageoptim.com
@@ -56,12 +66,16 @@ export VLC_URL=http://superb-dca2.dl.sourceforge.net/project/vlc/2.0.1/macosx/
 export VLC_FILE=vlc-2.0.1.dmg
 export PG_ADMIN_URL=http://ftp.postgresql.org/pub/pgadmin3/release/v1.14.2/osx
 export PG_ADMIN_FILE=pgadmin3-1.14.2.dmg
+export BILLINGS_URL=http://download.marketcircle.com/billings
+export BILLINGS_FILE=Billings372.zip
 export MONEYWELL_URL=http://downloads.nothirst.com
 export MONEYWELL_FILE=MoneyWell_17.zip
 export DELICIOUS_LIBRARY_URL=http://www.delicious-monster.com/downloads
 export DELICIOUS_LIBRARY_FILE=DeliciousLibrary2.zip
 export OPEN_OFFICE_URL=http://voxel.dl.sourceforge.net/project/openofficeorg.mirror/stable/3.3.0
 export OPEN_OFFICE_FILE=OOo_3.3.0_MacOS_x86_install_en-US.dmg
+export COLLOQUY_URL=http://colloquy.info/downloads
+export COLLOQUY_FILE=colloquy-latest.zip
 export SKYPE_URL=http://download.skype.com/macosx
 export SKYPE_FILE=Skype_5.7.59.1037.dmg
 
@@ -119,9 +133,6 @@ brew install redis
 cp /usr/local/Cellar/redis/$REDIS_VERSION/io.redis.redis-server.plist ~/Library/LaunchAgents/
 launchctl load -w ~/Library/LaunchAgents/io.redis.redis-server.plist
 
-# VirtualBox
-install_dmg_pkg $VIRTUAL_BOX_URL $VIRTUAL_BOX_FILE "VirtualBox"
-
 # Dropbox
 install_dmg_app $DROPBOX_URL $DROPBOX_FILE "Dropbox Installer"
 
@@ -149,6 +160,15 @@ install_dmg_app $CHROME_URL $CHROME_FILE "Google Chrome"
 # Opera
 install_dmg_app $OPERA_URL $OPERA_FILE "Opera"
 
+# OmniFocus
+install_dmg_app $OMNIFOCUS_URL $OMNIFOCUS_FILE "OmniFocus"
+
+# OmniOutliner
+install_dmg_app $OMNIOUTLINER_URL $OMNIOUTLINER_FILE "OmniOutliner"
+
+# OmniGraffle
+install_dmg_app $OMNIGRAFFLE_URL $OMNIGRAFFLE_FILE "OmniGrafflePro"
+
 # OmniDazzle
 install_dmg_app $OMNIDAZZLE_URL $OMNIDAZZLE_FILE "OmniDazzle"
 
@@ -157,6 +177,9 @@ install_zip_app $EXPANDRIVE_URL $EXPANDRIVE_FILE
 
 # iStat Menus (the app is the installer)
 download_only $ISTATS_URL $ISTATS_FILE
+
+# Air Display
+install_dmg_pkg $AIR_DISPLAY_URL $AIR_DISPLAY_FILE "Air Display Installer"
 
 # Pastebot Sync (preference pane)
 download_only $PASTEBOT_SYNC_URL $PASTEBOT_SYNC_FILE
@@ -173,6 +196,9 @@ install_dmg_app $ESPRESSO_URL $ESPRESSO_FILE "Espresso 2.0.1"
 # CodeKit
 install_zip_app $CODEKIT_URL $CODEKIT_FILE
 
+# Acorn
+install_zip_app $ACORN_URL $ACORN_FILE
+
 # DoubleTake
 install_dmg_app $DOUBLETAKE_URL $DOUBLETAKE_FILE "DoubleTake"
 
@@ -188,6 +214,9 @@ install_dmg_app $VLC_URL $VLC_FILE "vlc-2.0.1"
 # PG Admin
 install_dmg_app $PG_ADMIN_URL $PG_ADMIN_FILE "pgAdmin3"
 
+# Billings
+install_zip_app $BILLINGS_URL $BILLINGS_FILE
+
 # MoneyWell
 install_zip_app $MONEYWELL_URL $MONEYWELL_FILE
 
@@ -197,5 +226,11 @@ install_zip_app $DELICIOUS_LIBRARY_URL $DELICIOUS_LIBRARY_FILE
 # OpenOffice
 install_dmg_app $OPEN_OFFICE_URL $OPEN_OFFICE_FILE "OpenOffice.org 3.3"
 
+# Colloquy
+install_zip_app $COLLOQUY_URL $COLLOQUY_FILE
+
 # Skype
 install_dmg_app $SKYPE_URL $SKYPE_FILE "Skype"
+
+# VirtualBox
+install_dmg_pkg $VIRTUAL_BOX_URL $VIRTUAL_BOX_FILE "VirtualBox"

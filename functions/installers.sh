ee8a1ec96e82883307f8acaf881c6231488b0cb5
Brooke Kuhlmann <brooke@redalchemist.com>
2013-02-17 11:36:34 -0700

    Added verify_install function and forced all install definitions to define actual app name.

--- functions/installers.sh
+++ functions/installers.sh
@@ -3,10 +3,24 @@
 # DESCRIPTION
 # Installs and configures OSX software.
 
+# Verifies the install exists and completed successfully.
+# Parameters:
+# $1 = The application name.
+function verify_install {
+  application="/Applications/$1.app"
+  if [ -e "$application" ]; then
+    echo "$application install complete."
+  else
+    echo "ERROR: $application not found. Existing."
+    exit 1
+  fi
+}
+export -f verify_install
+
 # Downloads an installer to local disk.
 # Parameters:
-# $1 = url
-# $2 = file name
+# $1 = The remote URL.
+# $2 = The file name.
 function download_installer {
 	echo "Downloading $1/$2..."
 	rm -rf $WORK_PATH
@@ -19,8 +33,8 @@ export -f download_installer
 
 # Downloads an installer to the $HOME/Downloads folder for manual use.
 # Parameters:
-# $1 = url
-# $2 = file name
+# $1 = The remote URL.
+# $2 = The file name.
 function download_only {
 	echo "Downloading $1/$2..."
 	rm -rf $WORK_PATH
@@ -33,93 +47,117 @@ export -f download_only
 
 # Installs an application.
 # Parameters:
-# $1 = application path
+# $1 = The application path.
+# $2 = The application name.
 function install_app {
-	echo "Installing..."
-	find "$1" -type d -name "*.app" -exec sh -c 'cp -a "$0" /Applications/' {} ';'
+	app_name = "$2.app"
+	app_path = "/Applications/$app_name"
+
+  if [ -e "$app_path" ]; then
+    echo "$app_path exists, skipping."
+  else
+    echo "Installing $app_path..."
+    cp -a "$1/$app_name" "/Applications"
+  fi
+
+  verify_install "$2"
 }
 export -f install_app
 
 # Installs a package.
 # Parameters:
-# $1 = package path
+# $1 = The package path.
+# $2 = The application name.
 function install_pkg {
-	echo "Installing..."
-	find "$1" -type f -name "*.pkg" -o -name "*.mpkg" -exec sh -c 'sudo installer -pkg "$0" -target /' {} ';'
+  app_name = "$2.app"
+  app_path = "/Applications/$app_name"
+
+  if [ -e "$app_path" ]; then
+    echo "$app_path exists, skipping."
+  else
+    echo "Installing $app_path..."
+    find "$1" -type f -name "*.pkg" -o -name "*.mpkg" -exec sh -c 'sudo installer -pkg "$0" -target /' {} ';'
+  fi
+
+  verify_install "$2"  
 }
 export -f install_pkg
 
 # Installs a package via a DMG file.
 # Parameters:
-# $1 = URL
-# $2 = download file name
-# $3 = mount path
+# $1 = The remote URL.
+# $2 = The download file name.
+# $3 = The mount path.
+# $4 = The application name.
 function install_dmg_pkg {
-	mount_point="/Volumes/$3"
+  mount_point="/Volumes/$3"
 
-	download_installer $1 $2
-	download_file="$WORK_PATH/$2"
+  download_installer $1 $2
+  download_file="$WORK_PATH/$2"
 
-	echo "Mounting..."
-	hdiutil attach "$download_file" -noidmereveal
+  echo "Mounting..."
+  hdiutil attach "$download_file" -noidmereveal
 
-	install_pkg "$mount_point"
+  install_pkg "$mount_point" "$4"
 
-	echo "Cleaning..."
-	hdiutil detach -force "$mount_point"
-	rm -f $download_file
+  echo "Cleaning..."
+  hdiutil detach -force "$mount_point"
+  rm -f $download_file
 }
 export -f install_dmg_pkg
 
 # Installs an application via a DMG file.
 # Parameters:
-# $1 = URL
-# $2 = download file name
-# $3 = mount path
+# $1 = The remote URL.
+# $2 = The download file name.
+# $3 = The mount path.
+# $4 = The application name.
 function install_dmg_app {
-	mount_point="/Volumes/$3"
+  mount_point="/Volumes/$3"
 
-	download_installer $1 $2
-	download_file="$WORK_PATH/$2"
+  download_installer $1 $2
+  download_file="$WORK_PATH/$2"
 
-	echo "Mounting..."
-	hdiutil attach "$download_file" -noidmereveal
+  echo "Mounting..."
+  hdiutil attach "$download_file" -noidmereveal
 
-	install_app "$mount_point"
+  install_app "$mount_point" "$4"
 
-	echo "Cleaning..."
-	hdiutil detach -force "$mount_point"
-	rm -f $download_file
+  echo "Cleaning..."
+  hdiutil detach -force "$mount_point"
+  rm -f $download_file
 }
 export -f install_dmg_app
 
 # Installs an application via a zip file.
 # Parameters:
-# $1 = URL
-# $2 = download file name
+# $1 = The remote URL.
+# $2 = The download file name.
+# $3 = The application name.
 function install_zip_app {
-	download_installer $1 $2
+  download_installer $1 $2
 
-	echo "Preparing..."
-	cd $WORK_PATH
-	unzip "$2"
+  echo "Preparing..."
+  cd "$WORK_PATH"
+  unzip "$2"
 
-	install_app $WORK_PATH
+  install_app "$WORK_PATH" "$3"
 }
 export -f install_zip_app
 
 # Installs an application via a tar file.
 # Parameters:
-# $1 = URL
-# $2 = download file name
-# $3 = uncompress options
+# $1 = The remote URL.
+# $2 = The download file name.
+# $3 = The uncompress options.
+# $4 = The application name.
 function install_tar_app {
-	download_installer $1 $2
+  download_installer $1 $2
 
-	echo "Preparing..."
-	cd $WORK_PATH
-	tar "$3" "$2"
+  echo "Preparing..."
+  cd "$WORK_PATH"
+  tar "$3" "$2"
 
-	install_app $WORK_PATH
+  install_app "$WORK_PATH" "$4"
 }
 export -f install_tar_app
--- scripts/software.sh
+++ scripts/software.sh
@@ -82,125 +82,125 @@ cp /usr/local/Cellar/redis/*/homebrew.mxcl.redis.plist $HOME/Library/LaunchAgent
 launchctl load -w $HOME/Library/LaunchAgents/homebrew.mxcl.redis.plist
 
 # Caffeine
-install_zip_app $CAFFEINE_URL $CAFFEINE_FILE
+install_zip_app $CAFFEINE_URL $CAFFEINE_FILE $CAFFEINE_NAME
 
 # Dropbox
-install_dmg_app $DROPBOX_URL $DROPBOX_FILE "Dropbox Installer"
+install_dmg_app $DROPBOX_URL $DROPBOX_FILE "Dropbox Installer" $DROPBOX_NAME
 
 # Knox
-install_zip_app $KNOX_URL $KNOX_FILE
+install_zip_app $KNOX_URL $KNOX_FILE $KNOX_NAME
 
 # iTerm
-install_zip_app $ITERM_URL $ITERM_FILE
+install_zip_app $ITERM_URL $ITERM_FILE $ITERM_NAME
 
 # Sublime Text 2
-install_dmg_app $SUBLIME_TEXT_URL $SUBLIME_TEXT_FILE "Sublime Text 2"
+install_dmg_app $SUBLIME_TEXT_URL $SUBLIME_TEXT_FILE "Sublime Text 2" $SUBLIME_TEXT_NAME
 sudo ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/bin/sublime
 
 # Alfred
-install_dmg_app $ALFRED_URL $ALFRED_FILE "Alfred.app"
+install_dmg_app $ALFRED_URL $ALFRED_FILE "Alfred.app" $ALFRED_NAME
 
 # TextExpander
-install_zip_app $TEXTEXPANDER_URL $TEXTEXPANDER_FILE "TextExpander.app"
+install_zip_app $TEXTEXPANDER_URL $TEXTEXPANDER_FILE "TextExpander.app" $TEXTEXPANDER_NAME
 
 # Path Finder
-install_dmg_app $PATH_FINDER_URL $PATH_FINDER_FILE "Path Finder"
+install_dmg_app $PATH_FINDER_URL $PATH_FINDER_FILE "Path Finder" $PATH_FINDER_NAME
 
 # Firefox
-install_dmg_app $FIREFOX_URL $FIREFOX_FILE "Firefox"
+install_dmg_app $FIREFOX_URL $FIREFOX_FILE "Firefox" $FIREFOX_NAME
 
 # Google Chrome
-install_dmg_app $CHROME_URL $CHROME_FILE "Google Chrome"
+install_dmg_app $CHROME_URL $CHROME_FILE "Google Chrome" $CHROME_NAME
 
 # Opera
-install_dmg_app $OPERA_URL $OPERA_FILE "Opera"
+install_dmg_app $OPERA_URL $OPERA_FILE "Opera" $OPERA_NAME
 
 # OmniFocus
-install_dmg_app $OMNIFOCUS_URL $OMNIFOCUS_FILE "OmniFocus"
+install_dmg_app $OMNIFOCUS_URL $OMNIFOCUS_FILE "OmniFocus" $OMNIFOCUS_NAME
 
 # OmniOutliner
-install_dmg_app $OMNIOUTLINER_URL $OMNIOUTLINER_FILE "OmniOutliner"
+install_dmg_app $OMNIOUTLINER_URL $OMNIOUTLINER_FILE "OmniOutliner" $OMNIOUTLINER_NAME
 
 # OmniGraffle
-install_dmg_app $OMNIGRAFFLE_URL $OMNIGRAFFLE_FILE "OmniGrafflePro"
+install_dmg_app $OMNIGRAFFLE_URL $OMNIGRAFFLE_FILE "OmniGrafflePro" $OMNIGRAFFLE_NAME
 
 # OmniDazzle
-install_dmg_app $OMNIDAZZLE_URL $OMNIDAZZLE_FILE "OmniDazzle"
+install_dmg_app $OMNIDAZZLE_URL $OMNIDAZZLE_FILE "OmniDazzle" $OMNIDAZZLE_NAME
 
 # ExpanDrive
-install_zip_app $EXPANDRIVE_URL $EXPANDRIVE_FILE
+install_zip_app $EXPANDRIVE_URL $EXPANDRIVE_FILE $EXPANDRIVE_NAME
 
 # iStat Menus (the app is the installer)
-download_only $ISTATS_URL $ISTATS_FILE
+download_only $ISTATS_URL $ISTATS_FILE $ISTATS_NAME
 
 # Air Display
-install_dmg_pkg $AIR_DISPLAY_URL $AIR_DISPLAY_FILE "Air Display Installer"
+install_dmg_pkg $AIR_DISPLAY_URL $AIR_DISPLAY_FILE "Air Display Installer" $AIR_DISPLAY_NAME
 
 # Pastebot Sync (preference pane)
 download_only $PASTEBOT_SYNC_URL $PASTEBOT_SYNC_FILE
 
 # Bartender
-install_zip_app $BARTENDER_URL $BARTENDER_FILE
+install_zip_app $BARTENDER_URL $BARTENDER_FILE $BARTENDER_NAME
 
 # GitHub
-install_zip_app $GITHUB_URL $GITHUB_FILE
+install_zip_app $GITHUB_URL $GITHUB_FILE $GITHUB_NAME
 
 # GitX
-install_zip_app $GITX_URL $GITX_FILE
+install_zip_app $GITX_URL $GITX_FILE $GITX_NAME
 
 # Snippets
-install_zip_app $SNIPPETS_URL $SNIPPETS_FILE
+install_zip_app $SNIPPETS_URL $SNIPPETS_FILE $SNIPPETS_NAME
 
 # Espresso
-install_zip_app $ESPRESSO_URL $ESPRESSO_FILE
+install_zip_app $ESPRESSO_URL $ESPRESSO_FILE $ESPRESSO_NAME
 
 # CodeKit
-install_zip_app $CODEKIT_URL $CODEKIT_FILE
+install_zip_app $CODEKIT_URL $CODEKIT_FILE $CODEKIT_NAME
 
 # Acorn
-install_zip_app $ACORN_URL $ACORN_FILE
+install_zip_app $ACORN_URL $ACORN_FILE $ACORN_NAME
 
 # DoubleTake
-install_dmg_app $DOUBLETAKE_URL $DOUBLETAKE_FILE "DoubleTake"
+install_dmg_app $DOUBLETAKE_URL $DOUBLETAKE_FILE "DoubleTake" $DOUBLETAKE_NAME
 
 # ImageOptim
-install_tar_app $IMAGE_OPTIM_URL $IMAGE_OPTIM_FILE "-xjf"
+install_tar_app $IMAGE_OPTIM_URL $IMAGE_OPTIM_FILE "-xjf" $IMAGE_OPTIM_NAME
 
 # VLC
-install_dmg_app $VLC_URL $VLC_FILE "vlc-2.0.1"
+install_dmg_app $VLC_URL $VLC_FILE "vlc-2.0.1" $VLC_NAME
 
 # Induction
-install_zip_app $INDUCTION_URL $INDUCTION_FILE
+install_zip_app $INDUCTION_URL $INDUCTION_FILE $INDUCTION_NAME
 
 # PG Admin
-install_dmg_app $PG_ADMIN_URL $PG_ADMIN_FILE "pgAdmin3"
+install_dmg_app $PG_ADMIN_URL $PG_ADMIN_FILE "pgAdmin3" $PG_ADMIN_NAME
 
 # Namebench
-install_dmg_app $NAMEBENCH_URL $NAMEBENCH_FILE "namebench"
+install_dmg_app $NAMEBENCH_URL $NAMEBENCH_FILE "namebench" $NAMEBENCH_NAME
 
 # Billings
-install_zip_app $BILLINGS_URL $BILLINGS_FILE
+install_zip_app $BILLINGS_URL $BILLINGS_FILE $BILLINGS_NAME
 
 # MoneyWell
-install_zip_app $MONEYWELL_URL $MONEYWELL_FILE
+install_zip_app $MONEYWELL_URL $MONEYWELL_FILE $MONEYWELL_NAME
 
 # Delicious Library
-install_zip_app $DELICIOUS_LIBRARY_URL $DELICIOUS_LIBRARY_FILE
+install_zip_app $DELICIOUS_LIBRARY_URL $DELICIOUS_LIBRARY_FILE $DELICIOUS_LIBRARY_NAME
 
 # OpenOffice
-install_dmg_app $OPEN_OFFICE_URL $OPEN_OFFICE_FILE "OpenOffice.org 3.3"
+install_dmg_app $OPEN_OFFICE_URL $OPEN_OFFICE_FILE "OpenOffice.org 3.3" $OPEN_OFFICE_NAME
 
 # Colloquy
-install_zip_app $COLLOQUY_URL $COLLOQUY_FILE
+install_zip_app $COLLOQUY_URL $COLLOQUY_FILE $COLLOQUY_NAME
 
 # Skype
-install_dmg_app $SKYPE_URL $SKYPE_FILE "Skype"
+install_dmg_app $SKYPE_URL $SKYPE_FILE "Skype" $SKYPE_NAME
 
 # VirtualBox
-install_dmg_pkg $VIRTUAL_BOX_URL $VIRTUAL_BOX_FILE "VirtualBox"
+install_dmg_pkg $VIRTUAL_BOX_URL $VIRTUAL_BOX_FILE "VirtualBox" $VIRTUAL_BOX_NAME
 
 # Eye-Fi
-install_dmg_pkg $EYEFI_URL $EYEFI_FILE "Eye-Fi Installer"
+install_dmg_pkg $EYEFI_URL $EYEFI_FILE "Eye-Fi Installer" $EYEFI_NAME
 
 # Doxie
-install_dmg_pkg $DOXIE_URL $DOXIE_FILE "Doxie"
+install_dmg_pkg $DOXIE_URL $DOXIE_FILE "Doxie" $DOXIE_NAME

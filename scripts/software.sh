883e1ebf02983e9a23073470a18eef82f64e65f4
Brooke Kuhlmann <brooke@redalchemist.com>
2013-02-17 11:43:55 -0700

    Prefixed all *URL, *FILE, and *NAME variables with APP for better dynamic lookup.

--- scripts/software.sh
+++ scripts/software.sh
@@ -82,125 +82,125 @@ cp /usr/local/Cellar/redis/*/homebrew.mxcl.redis.plist $HOME/Library/LaunchAgent
 launchctl load -w $HOME/Library/LaunchAgents/homebrew.mxcl.redis.plist
 
 # Caffeine
-install_zip_app $CAFFEINE_URL $CAFFEINE_FILE $CAFFEINE_NAME
+install_zip_app $CAFFEINE_APP_URL $CAFFEINE_APP_FILE $CAFFEINE_APP_NAME
 
 # Dropbox
-install_dmg_app $DROPBOX_URL $DROPBOX_FILE "Dropbox Installer" $DROPBOX_NAME
+install_dmg_app $DROPBOX_APP_URL $DROPBOX_APP_FILE "Dropbox Installer" $DROPBOX_APP_NAME
 
 # Knox
-install_zip_app $KNOX_URL $KNOX_FILE $KNOX_NAME
+install_zip_app $KNOX_APP_URL $KNOX_APP_FILE $KNOX_APP_NAME
 
 # iTerm
-install_zip_app $ITERM_URL $ITERM_FILE $ITERM_NAME
+install_zip_app $ITERM_APP_URL $ITERM_APP_FILE $ITERM_APP_NAME
 
 # Sublime Text 2
-install_dmg_app $SUBLIME_TEXT_URL $SUBLIME_TEXT_FILE "Sublime Text 2" $SUBLIME_TEXT_NAME
+install_dmg_app $SUBLIME_TEXT_APP_URL $SUBLIME_TEXT_APP_FILE "Sublime Text 2" $SUBLIME_TEXT_APP_NAME
 sudo ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/bin/sublime
 
 # Alfred
-install_dmg_app $ALFRED_URL $ALFRED_FILE "Alfred.app" $ALFRED_NAME
+install_dmg_app $ALFRED_APP_URL $ALFRED_APP_FILE "Alfred.app" $ALFRED_APP_NAME
 
 # TextExpander
-install_zip_app $TEXTEXPANDER_URL $TEXTEXPANDER_FILE "TextExpander.app" $TEXTEXPANDER_NAME
+install_zip_app $TEXTEXPANDER_APP_URL $TEXTEXPANDER_APP_FILE "TextExpander.app" $TEXTEXPANDER_APP_NAME
 
 # Path Finder
-install_dmg_app $PATH_FINDER_URL $PATH_FINDER_FILE "Path Finder" $PATH_FINDER_NAME
+install_dmg_app $PATH_FINDER_APP_URL $PATH_FINDER_APP_FILE "Path Finder" $PATH_FINDER_APP_NAME
 
 # Firefox
-install_dmg_app $FIREFOX_URL $FIREFOX_FILE "Firefox" $FIREFOX_NAME
+install_dmg_app $FIREFOX_APP_URL $FIREFOX_APP_FILE "Firefox" $FIREFOX_APP_NAME
 
 # Google Chrome
-install_dmg_app $CHROME_URL $CHROME_FILE "Google Chrome" $CHROME_NAME
+install_dmg_app $CHROME_APP_URL $CHROME_APP_FILE "Google Chrome" $CHROME_APP_NAME
 
 # Opera
-install_dmg_app $OPERA_URL $OPERA_FILE "Opera" $OPERA_NAME
+install_dmg_app $OPERA_APP_URL $OPERA_APP_FILE "Opera" $OPERA_APP_NAME
 
 # OmniFocus
-install_dmg_app $OMNIFOCUS_URL $OMNIFOCUS_FILE "OmniFocus" $OMNIFOCUS_NAME
+install_dmg_app $OMNIFOCUS_APP_URL $OMNIFOCUS_APP_FILE "OmniFocus" $OMNIFOCUS_APP_NAME
 
 # OmniOutliner
-install_dmg_app $OMNIOUTLINER_URL $OMNIOUTLINER_FILE "OmniOutliner" $OMNIOUTLINER_NAME
+install_dmg_app $OMNIOUTLINER_APP_URL $OMNIOUTLINER_APP_FILE "OmniOutliner" $OMNIOUTLINER_APP_NAME
 
 # OmniGraffle
-install_dmg_app $OMNIGRAFFLE_URL $OMNIGRAFFLE_FILE "OmniGrafflePro" $OMNIGRAFFLE_NAME
+install_dmg_app $OMNIGRAFFLE_APP_URL $OMNIGRAFFLE_APP_FILE "OmniGrafflePro" $OMNIGRAFFLE_APP_NAME
 
 # OmniDazzle
-install_dmg_app $OMNIDAZZLE_URL $OMNIDAZZLE_FILE "OmniDazzle" $OMNIDAZZLE_NAME
+install_dmg_app $OMNIDAZZLE_APP_URL $OMNIDAZZLE_APP_FILE "OmniDazzle" $OMNIDAZZLE_APP_NAME
 
 # ExpanDrive
-install_zip_app $EXPANDRIVE_URL $EXPANDRIVE_FILE $EXPANDRIVE_NAME
+install_zip_app $EXPANDRIVE_APP_URL $EXPANDRIVE_APP_FILE $EXPANDRIVE_APP_NAME
 
 # iStat Menus (the app is the installer)
-download_only $ISTATS_URL $ISTATS_FILE $ISTATS_NAME
+download_only $ISTATS_APP_URL $ISTATS_APP_FILE $ISTATS_APP_NAME
 
 # Air Display
-install_dmg_pkg $AIR_DISPLAY_URL $AIR_DISPLAY_FILE "Air Display Installer" $AIR_DISPLAY_NAME
+install_dmg_pkg $AIR_DISPLAY_APP_URL $AIR_DISPLAY_APP_FILE "Air Display Installer" $AIR_DISPLAY_APP_NAME
 
 # Pastebot Sync (preference pane)
-download_only $PASTEBOT_SYNC_URL $PASTEBOT_SYNC_FILE
+download_only $PASTEBOT_SYNC_APP_URL $PASTEBOT_SYNC_APP_FILE
 
 # Bartender
-install_zip_app $BARTENDER_URL $BARTENDER_FILE $BARTENDER_NAME
+install_zip_app $BARTENDER_APP_URL $BARTENDER_APP_FILE $BARTENDER_APP_NAME
 
 # GitHub
-install_zip_app $GITHUB_URL $GITHUB_FILE $GITHUB_NAME
+install_zip_app $GITHUB_APP_URL $GITHUB_APP_FILE $GITHUB_APP_NAME
 
 # GitX
-install_zip_app $GITX_URL $GITX_FILE $GITX_NAME
+install_zip_app $GITX_APP_URL $GITX_APP_FILE $GITX_APP_NAME
 
 # Snippets
-install_zip_app $SNIPPETS_URL $SNIPPETS_FILE $SNIPPETS_NAME
+install_zip_app $SNIPPETS_APP_URL $SNIPPETS_APP_FILE $SNIPPETS_APP_NAME
 
 # Espresso
-install_zip_app $ESPRESSO_URL $ESPRESSO_FILE $ESPRESSO_NAME
+install_zip_app $ESPRESSO_APP_URL $ESPRESSO_APP_FILE $ESPRESSO_APP_NAME
 
 # CodeKit
-install_zip_app $CODEKIT_URL $CODEKIT_FILE $CODEKIT_NAME
+install_zip_app $CODEKIT_APP_URL $CODEKIT_APP_FILE $CODEKIT_APP_NAME
 
 # Acorn
-install_zip_app $ACORN_URL $ACORN_FILE $ACORN_NAME
+install_zip_app $ACORN_APP_URL $ACORN_APP_FILE $ACORN_APP_NAME
 
 # DoubleTake
-install_dmg_app $DOUBLETAKE_URL $DOUBLETAKE_FILE "DoubleTake" $DOUBLETAKE_NAME
+install_dmg_app $DOUBLETAKE_APP_URL $DOUBLETAKE_APP_FILE "DoubleTake" $DOUBLETAKE_APP_NAME
 
 # ImageOptim
-install_tar_app $IMAGE_OPTIM_URL $IMAGE_OPTIM_FILE "-xjf" $IMAGE_OPTIM_NAME
+install_tar_app $IMAGE_OPTIM_APP_URL $IMAGE_OPTIM_APP_FILE "-xjf" $IMAGE_OPTIM_APP_NAME
 
 # VLC
-install_dmg_app $VLC_URL $VLC_FILE "vlc-2.0.1" $VLC_NAME
+install_dmg_app $VLC_APP_URL $VLC_APP_FILE "vlc-2.0.1" $VLC_APP_NAME
 
 # Induction
-install_zip_app $INDUCTION_URL $INDUCTION_FILE $INDUCTION_NAME
+install_zip_app $INDUCTION_APP_URL $INDUCTION_APP_FILE $INDUCTION_APP_NAME
 
 # PG Admin
-install_dmg_app $PG_ADMIN_URL $PG_ADMIN_FILE "pgAdmin3" $PG_ADMIN_NAME
+install_dmg_app $PG_ADMIN_APP_URL $PG_ADMIN_APP_FILE "pgAdmin3" $PG_ADMIN_APP_NAME
 
 # Namebench
-install_dmg_app $NAMEBENCH_URL $NAMEBENCH_FILE "namebench" $NAMEBENCH_NAME
+install_dmg_app $NAMEBENCH_APP_URL $NAMEBENCH_APP_FILE "namebench" $NAMEBENCH_APP_NAME
 
 # Billings
-install_zip_app $BILLINGS_URL $BILLINGS_FILE $BILLINGS_NAME
+install_zip_app $BILLINGS_APP_URL $BILLINGS_APP_FILE $BILLINGS_APP_NAME
 
 # MoneyWell
-install_zip_app $MONEYWELL_URL $MONEYWELL_FILE $MONEYWELL_NAME
+install_zip_app $MONEYWELL_APP_URL $MONEYWELL_APP_FILE $MONEYWELL_APP_NAME
 
 # Delicious Library
-install_zip_app $DELICIOUS_LIBRARY_URL $DELICIOUS_LIBRARY_FILE $DELICIOUS_LIBRARY_NAME
+install_zip_app $DELICIOUS_LIBRARY_APP_URL $DELICIOUS_LIBRARY_APP_FILE $DELICIOUS_LIBRARY_APP_NAME
 
 # OpenOffice
-install_dmg_app $OPEN_OFFICE_URL $OPEN_OFFICE_FILE "OpenOffice.org 3.3" $OPEN_OFFICE_NAME
+install_dmg_app $OPEN_OFFICE_APP_URL $OPEN_OFFICE_APP_FILE "OpenOffice.org 3.3" $OPEN_OFFICE_APP_NAME
 
 # Colloquy
-install_zip_app $COLLOQUY_URL $COLLOQUY_FILE $COLLOQUY_NAME
+install_zip_app $COLLOQUY_APP_URL $COLLOQUY_APP_FILE $COLLOQUY_APP_NAME
 
 # Skype
-install_dmg_app $SKYPE_URL $SKYPE_FILE "Skype" $SKYPE_NAME
+install_dmg_app $SKYPE_APP_URL $SKYPE_APP_FILE "Skype" $SKYPE_APP_NAME
 
 # VirtualBox
-install_dmg_pkg $VIRTUAL_BOX_URL $VIRTUAL_BOX_FILE "VirtualBox" $VIRTUAL_BOX_NAME
+install_dmg_pkg $VIRTUAL_BOX_APP_URL $VIRTUAL_BOX_APP_FILE "VirtualBox" $VIRTUAL_BOX_APP_NAME
 
 # Eye-Fi
-install_dmg_pkg $EYEFI_URL $EYEFI_FILE "Eye-Fi Installer" $EYEFI_NAME
+install_dmg_pkg $EYEFI_APP_URL $EYEFI_APP_FILE "Eye-Fi Installer" $EYEFI_APP_NAME
 
 # Doxie
-install_dmg_pkg $DOXIE_URL $DOXIE_FILE "Doxie" $DOXIE_NAME
+install_dmg_pkg $DOXIE_APP_URL $DOXIE_APP_FILE "Doxie" $DOXIE_APP_NAME
--- settings/settings.sh
+++ settings/settings.sh
@@ -16,165 +16,165 @@ export WORK_PATH=/private/tmp/downloads # Temporary location for processing of f
 export RUBY_VERSION=1.9.3-p385
 
 # Software
-export CAFFEINE_URL=http://lightheadsw.com/files/releases/com.lightheadsw.Caffeine
-export CAFFEINE_FILE=Caffeine1.1.1.zip
-export CAFFEINE_NAME=Caffeine
+export CAFFEINE_APP_URL=http://lightheadsw.com/files/releases/com.lightheadsw.Caffeine
+export CAFFEINE_APP_FILE=Caffeine1.1.1.zip
+export CAFFEINE_APP_NAME=Caffeine
 
-export DROPBOX_URL=https://d1ilhw0800yew8.cloudfront.net/client
-export DROPBOX_FILE=Dropbox%201.6.16.dmg
-export DROPBOX_NAME=Dropbox
+export DROPBOX_APP_URL=https://d1ilhw0800yew8.cloudfront.net/client
+export DROPBOX_APP_FILE=Dropbox%201.6.16.dmg
+export DROPBOX_APP_NAME=Dropbox
 
-export KNOX_URL=https://d13itkw33a7sus.cloudfront.net/dist/K
-export KNOX_FILE=Knox-2.2.0.zip
-export KNOX_NAME=Knox
+export KNOX_APP_URL=https://d13itkw33a7sus.cloudfront.net/dist/K
+export KNOX_APP_FILE=Knox-2.2.0.zip
+export KNOX_APP_NAME=Knox
 
-export ITERM_URL=https://iterm2.googlecode.com/files
-export ITERM_FILE=iTerm2-1_0_0_20130122.zip
-export ITERM_NAME=iTerm
+export ITERM_APP_URL=https://iterm2.googlecode.com/files
+export ITERM_APP_FILE=iTerm2-1_0_0_20130122.zip
+export ITERM_APP_NAME=iTerm
 
-export SUBLIME_TEXT_URL=http://c758482.r82.cf2.rackcdn.com
-export SUBLIME_TEXT_FILE=Sublime%20Text%202.0.1.dmg
-export SUBLIME_TEXT_NAME="Sublime Text 2"
+export SUBLIME_TEXT_APP_URL=http://c758482.r82.cf2.rackcdn.com
+export SUBLIME_TEXT_APP_FILE=Sublime%20Text%202.0.1.dmg
+export SUBLIME_TEXT_APP_NAME="Sublime Text 2"
 
-export ALFRED_URL=http://cachefly.alfredapp.com
-export ALFRED_FILE=alfred_1.3.2_265.zip
-export ALFRED_NAME=Alfred
+export ALFRED_APP_URL=http://cachefly.alfredapp.com
+export ALFRED_APP_FILE=alfred_1.3.2_265.zip
+export ALFRED_APP_NAME=Alfred
 
-export TEXTEXPANDER_URL=http://cdn.smilesoftware.com
-export TEXTEXPANDER_FILE=TextExpander_4.0.5.zip
-export TEXTEXPANDER_NAME=TextExpander
+export TEXTEXPANDER_APP_URL=http://cdn.smilesoftware.com
+export TEXTEXPANDER_APP_FILE=TextExpander_4.0.5.zip
+export TEXTEXPANDER_APP_NAME=TextExpander
 
-export PATH_FINDER_URL=http://get.cocoatech.com
-export PATH_FINDER_FILE=PF6_LION.zip
-export PATH_FINDER_NAME="Path Finder"
+export PATH_FINDER_APP_URL=http://get.cocoatech.com
+export PATH_FINDER_APP_FILE=PF6_LION.zip
+export PATH_FINDER_APP_NAME="Path Finder"
 
-export FIREFOX_URL=http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/18.0.2/mac/en-US
-export FIREFOX_FILE=Firefox%2018.0.2.dmg
-export FIREFOX_NAME=Firefox
+export FIREFOX_APP_URL=http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/18.0.2/mac/en-US
+export FIREFOX_APP_FILE=Firefox%2018.0.2.dmg
+export FIREFOX_APP_NAME=Firefox
 
-export CHROME_URL=https://dl.google.com/chrome/mac/stable/GGRO
-export CHROME_FILE=googlechrome.dmg
-export CHROME_NAME="Google Chrome"
+export CHROME_APP_URL=https://dl.google.com/chrome/mac/stable/GGRO
+export CHROME_APP_FILE=googlechrome.dmg
+export CHROME_APP_NAME="Google Chrome"
 
-export OPERA_URL=http://get3.opera.com/pub/opera/mac/1214
-export OPERA_FILE=Opera_12.14_Setup_Intel.dmg
-export OPERA_NAME=Opera
+export OPERA_APP_URL=http://get3.opera.com/pub/opera/mac/1214
+export OPERA_APP_FILE=Opera_12.14_Setup_Intel.dmg
+export OPERA_APP_NAME=Opera
 
-export OMNIFOCUS_URL=http://downloads2.omnigroup.com/software/MacOSX/10.6
-export OMNIFOCUS_FILE=OmniFocus-1.10.4.dmg
-export OMNIFOCUS_NAME=OmniFocus
+export OMNIFOCUS_APP_URL=http://downloads2.omnigroup.com/software/MacOSX/10.6
+export OMNIFOCUS_APP_FILE=OmniFocus-1.10.4.dmg
+export OMNIFOCUS_APP_NAME=OmniFocus
 
-export OMNIOUTLINER_URL=http://downloads2.omnigroup.com/software/MacOSX/10.4
-export OMNIOUTLINER_FILE=OmniOutliner-3.10.6.dmg
-export OMNIOUTLINER_NAME=OmniOutliner
+export OMNIOUTLINER_APP_URL=http://downloads2.omnigroup.com/software/MacOSX/10.4
+export OMNIOUTLINER_APP_FILE=OmniOutliner-3.10.6.dmg
+export OMNIOUTLINER_APP_NAME=OmniOutliner
 
-export OMNIGRAFFLE_URL=http://downloads2.omnigroup.com/software/MacOSX/10.6
-export OMNIGRAFFLE_FILE=OmniGrafflePro-5.4.2.dmg
-export OMNIGRAFFLE_NAME="OmniGraffle Professional 5"
+export OMNIGRAFFLE_APP_URL=http://downloads2.omnigroup.com/software/MacOSX/10.6
+export OMNIGRAFFLE_APP_FILE=OmniGrafflePro-5.4.2.dmg
+export OMNIGRAFFLE_APP_NAME="OmniGraffle Professional 5"
 
-export OMNIDAZZLE_URL=http://downloads2.omnigroup.com/software/MacOSX/10.6
-export OMNIDAZZLE_FILE=OmniDazzle-1.2.dmg
-export OMNIDAZZLE_NAME=OmniDazzle
+export OMNIDAZZLE_APP_URL=http://downloads2.omnigroup.com/software/MacOSX/10.6
+export OMNIDAZZLE_APP_FILE=OmniDazzle-1.2.dmg
+export OMNIDAZZLE_APP_NAME=OmniDazzle
 
-export EXPANDRIVE_URL=http://downloads.expandrive.com/expandrive/v2-3-1_published_2012-03-07_at_15_18_52
-export EXPANDRIVE_FILE=ExpanDrive.zip
-export EXPANDRIVE_NAME=ExpanDrive
+export EXPANDRIVE_APP_URL=http://downloads.expandrive.com/expandrive/v2-3-1_published_2012-03-07_at_15_18_52
+export EXPANDRIVE_APP_FILE=ExpanDrive.zip
+export EXPANDRIVE_APP_NAME=ExpanDrive
 
-export ISTATS_URL=http://s3.amazonaws.com/bjango/files/istatmenus4
-export ISTATS_FILE=istatmenus4.03.zip
-export ISTATS_NAME="iStat Menus"
+export ISTATS_APP_URL=http://s3.amazonaws.com/bjango/files/istatmenus4
+export ISTATS_APP_FILE=istatmenus4.03.zip
+export ISTATS_APP_NAME="iStat Menus"
 
-export AIR_DISPLAY_URL=http://avatron.com/downloads
-export AIR_DISPLAY_FILE=AirDisplayInstaller.dmg
-export AIR_DISPLAY_NAME="Air Display"
+export AIR_DISPLAY_APP_URL=http://avatron.com/downloads
+export AIR_DISPLAY_APP_FILE=AirDisplayInstaller.dmg
+export AIR_DISPLAY_APP_NAME="Air Display"
 
-export PASTEBOT_SYNC_URL=http://tapbots.net/pastebot
-export PASTEBOT_SYNC_FILE=PastebotSync.dmg
+export PASTEBOT_SYNC_APP_URL=http://tapbots.net/pastebot
+export PASTEBOT_SYNC_APP_FILE=PastebotSAPP_ync.dmg
 
-export BARTENDER_URL=http://www.macbartender.com/Demo
-export BARTENDER_SYNC_FILE=Bartender.zip
-export BARTENDER_SYNC_NAME=Bartender
+export BARTENDER_APP_URL=http://www.macbartender.com/Demo
+export BARTENDER_SYNC_APP_FILE=Bartender.zip
+export BARTENDER_SYNC_APP_NAME=Bartender
 
-export GITHUB_URL=https://github-central.s3.amazonaws.com
-export GITHUB_FILE=mac%2FGitHub%20for%20Mac%20107.zip
-export GITHUB_NAME=GitHub
+export GITHUB_APP_URL=https://github-central.s3.amazonaws.com
+export GITHUB_APP_FILE=mac%2FGitHub%20for%20Mac%20107.zip
+export GITHUB_APP_NAME=GitHub
 
-export GITX_URL=https://github.com/downloads/laullon/gitx
-export GITX_FILE=GitX-L_v0.8.4.zip
-export GITX_NAME=GitX
+export GITX_APP_URL=https://github.com/downloads/laullon/gitx
+export GITX_APP_FILE=GitX-L_v0.8.4.zip
+export GITX_APP_NAME=GitX
 
-export SNIPPETS_URL=http://www.snippetsapp.com/download
-export SNIPPETS_FILE=Snippets-1.4.1.zip
-export SNIPPETS_NAME=Snippets
+export SNIPPETS_APP_URL=http://www.snippetsapp.com/download
+export SNIPPETS_APP_FILE=Snippets-1.4.1.zip
+export SNIPPETS_APP_NAME=Snippets
 
-export ESPRESSO_URL=https://s3.amazonaws.com/macrabbit/downloads
-export ESPRESSO_FILE=Espresso%202.0.5.zip
-export ESPRESSO_NAME=Espresso
+export ESPRESSO_APP_URL=https://s3.amazonaws.com/macrabbit/downloads
+export ESPRESSO_APP_FILE=Espresso%202.0.5.zip
+export ESPRESSO_APP_NAME=Espresso
 
-export CODEKIT_URL=http://incident57.com/codekit/files
-export CODEKIT_FILE=codekit-8248.zip
-export CODEKIT_NAME=CodeKit
+export CODEKIT_APP_URL=http://incident57.com/codekit/files
+export CODEKIT_APP_FILE=codekit-8248.zip
+export CODEKIT_APP_NAME=CodeKit
 
-export ACORN_URL=http://flyingmeat.com/download
-export ACORN_FILE=Acorn.zip
-export ACORN_NAME=Acorn
+export ACORN_APP_URL=http://flyingmeat.com/download
+export ACORN_APP_FILE=Acorn.zip
+export ACORN_APP_NAME=Acorn
 
-export DOUBLETAKE_URL=http://echoone.com/doubletake
-export DOUBLETAKE_FILE=DoubleTake.dmg
-export DOUBLETAKE_NAME=DoubleTake
+export DOUBLETAKE_APP_URL=http://echoone.com/doubletake
+export DOUBLETAKE_APP_FILE=DoubleTake.dmg
+export DOUBLETAKE_APP_NAME=DoubleTake
 
-export IMAGE_OPTIM_URL=http://imageoptim.com
-export IMAGE_OPTIM_FILE=ImageOptim.tbz2
-export IMAGE_OPTIM_NAME=ImageOptim
+export IMAGE_OPTIM_APP_URL=http://imageoptim.com
+export IMAGE_OPTIM_APP_FILE=ImageOptim.tbz2
+export IMAGE_OPTIM_APP_NAME=ImageOptim
 
-export VLC_URL=http://hivelocity.dl.sourceforge.net/project/vlc/2.0.5/macosx
-export VLC_FILE=vlc-2.0.5.dmg
-export VLC_NAME=VLC
+export VLC_APP_URL=http://hivelocity.dl.sourceforge.net/project/vlc/2.0.5/macosx
+export VLC_APP_FILE=vlc-2.0.5.dmg
+export VLC_APP_NAME=VLC
 
-export INDUCTION_URL=https://mesmerize.s3.amazonaws.com/Induction
-export INDUCTION_FILE=Induction-28.zip
-export INDUCTION_NAME=Induction
+export INDUCTION_APP_URL=https://mesmerize.s3.amazonaws.com/Induction
+export INDUCTION_APP_FILE=Induction-28.zip
+export INDUCTION_APP_NAME=Induction
 
-export PG_ADMIN_URL=http://ftp.postgresql.org/pub/pgadmin3/release/v1.16.1/osx
-export PG_ADMIN_FILE=pgadmin3-1.16.1.dmg
-export PG_ADMIN_NAME=pgAdmin3
+export PG_ADMIN_APP_URL=http://ftp.postgresql.org/pub/pgadmin3/release/v1.16.1/osx
+export PG_ADMIN_APP_FILE=pgadmin3-1.16.1.dmg
+export PG_ADMIN_APP_NAME=pgAdmin3
 
-export NAMEBENCH_URL=http://namebench.googlecode.com/files/namebench-1.3.1-Mac_OS_X.dmg
-export NAMEBENCH_FILE=namebench-1.3.1-Mac_OS_X.dmg
-export NAMEBENCH_NAME=Namebench
+export NAMEBENCH_APP_URL=http://namebench.googlecode.com/files/namebench-1.3.1-Mac_OS_X.dmg
+export NAMEBENCH_APP_FILE=namebench-1.3.1-Mac_OS_X.dmg
+export NAMEBENCH_APP_NAME=Namebench
 
-export BILLINGS_URL=http://download.marketcircle.com/billings
-export BILLINGS_FILE=Billings376.zip
-export BILLINGS_NAME=Billings
+export BILLINGS_APP_URL=http://download.marketcircle.com/billings
+export BILLINGS_APP_FILE=Billings376.zip
+export BILLINGS_APP_NAME=Billings
 
-export MONEYWELL_URL=http://downloads.nothirst.com
-export MONEYWELL_FILE=MoneyWell_17.zip
-export MONEYWELL_NAME=MoneyWell
+export MONEYWELL_APP_URL=http://downloads.nothirst.com
+export MONEYWELL_APP_FILE=MoneyWell_17.zip
+export MONEYWELL_APP_NAME=MoneyWell
 
-export DELICIOUS_LIBRARY_URL=http://www.delicious-monster.com/downloads
-export DELICIOUS_LIBRARY_FILE=DeliciousLibrary2.zip
-export DELICIOUS_LIBRARY_NAME="Delicious Library 2"
+export DELICIOUS_LIBRARY_APP_URL=http://www.delicious-monster.com/downloads
+export DELICIOUS_LIBRARY_APP_FILE=DeliciousLibrary2.zip
+export DELICIOUS_LIBRARY_APP_NAME="Delicious Library 2"
 
-export OPEN_OFFICE_URL=http://hivelocity.dl.sourceforge.net/project/openofficeorg.mirror/stable/3.4.1
-export OPEN_OFFICE_FILE=Apache_OpenOffice_incubating_3.4.1_MacOS_x86_install_en-US.dmg
-export OPEN_OFFICE_NAME=OpenOffice.org
+export OPEN_OFFICE_APP_URL=http://hivelocity.dl.sourceforge.net/project/openofficeorg.mirror/stable/3.4.1
+export OPEN_OFFICE_APP_FILE=Apache_OpenOffice_incubating_3.4.1_MacOS_x86_install_en-US.dmg
+export OPEN_OFFICE_APP_NAME=OpenOffice.org
 
-export COLLOQUY_URL=http://colloquy.info/downloads
-export COLLOQUY_FILE=colloquy-latest.zip
-export COLLOQUY_NAME=Colloquy
+export COLLOQUY_APP_URL=http://colloquy.info/downloads
+export COLLOQUY_APP_FILE=colloquy-latest.zip
+export COLLOQUY_APP_NAME=Colloquy
 
-export SKYPE_URL=http://download.skype.com/macosx
-export SKYPE_FILE=Skype_6.2.0.1117.dmg
-export SKYPE_NAME=Skype
+export SKYPE_APP_URL=http://download.skype.com/macosx
+export SKYPE_APP_FILE=Skype_6.2.0.1117.dmg
+export SKYPE_APP_NAME=Skype
 
-export VIRTUAL_BOX_URL=http://download.virtualbox.org/virtualbox/4.2.6
-export VIRTUAL_BOX_FILE=VirtualBox-4.2.6-82870-OSX.dmg
-export VIRTUAL_BOX_NAME=VirtualBox
+export VIRTUAL_BOX_APP_URL=http://download.virtualbox.org/virtualbox/4.2.6
+export VIRTUAL_BOX_APP_FILE=VirtualBox-4.2.6-82870-OSX.dmg
+export VIRTUAL_BOX_APP_NAME=VirtualBox
 
-export EYEFI_URL=http://support.eye.fi/agent/download/3.4.29
-export EYEFI_FILE=Eye-Fi.dmg
-export EYEFI_NAME="Eye-Fi Center"
+export EYEFI_APP_URL=http://support.eye.fi/agent/download/3.4.29
+export EYEFI_APP_FILE=Eye-Fi.dmg
+export EYEFI_APP_NAME="Eye-Fi Center"
 
-export DOXIE_URL=http://www.getdoxie.com/resources/files
-export DOXIE_FILE=Doxie.dmg
-export DOXIE_NAME=Doxie
+export DOXIE_APP_URL=http://www.getdoxie.com/resources/files
+export DOXIE_APP_FILE=Doxie.dmg
+export DOXIE_APP_NAME=Doxie
